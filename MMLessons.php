70067e483bef580c2b0dfeb6fd81c147e6f0c188
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-09-22 11:17:08 -0600

    lessons2

--- MMLessons.php
+++ MMLessons.php
@@ -3,9 +3,12 @@
 	
 	<body>
 		<table>
-			This is where there will be a list of all of the lessons
-			that have been submitted by people.
+			<tr>
+				<td>
+				This is where there will be a list of all of the lessons
+				that have been submitted by people.
+				</td>
+			</tr>
 		</table>
-	
 	</body>
 </html>
