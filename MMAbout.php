a24cb3525aee7a085640da459b89efdfc412dc2b
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-09-22 10:37:31 -0600

    about4

--- MMAbout.php
+++ MMAbout.php
@@ -2,22 +2,26 @@
 	<?php require_once "MMHeader.php"; ?>
 	
 	<body>
-		<div class="about">
-			This website is a place for math teachers to share ideas
-			on awesome lessons that they use in their classrooms. One
-			of the biggest phrases that teachers always hear when it
-			comes to lesson creation is, "Do not recreate the wheel".
-			So this is a place to help you to not recreate the wheel!
-		</div>
+		<table>
+		<tr>
+			<div class="about">
+				This website is a place for math teachers to share ideas
+				on awesome lessons that they use in their classrooms. One
+				of the biggest phrases that teachers always hear when it
+				comes to lesson creation is, "Do not recreate the wheel".
+				So this is a place to help you to not recreate the wheel!
+			</div>
 		
-		<div class="contact">
-			<form>
-				Name:
-				<input type="text"><br>
-				Email:
-				<input type="text"><br>
-				Message:<br>
-				<textarea id="message"></textarea>
-		</div>
+			<div class="contact">
+				<form>
+					Name:
+					<input type="text"><br>
+					Email:
+					<input type="text"><br>
+					Message:
+					<textarea id="message"></textarea>
+			</div>
+		</tr>
+		</table>
 	</body>
 </html>
