90f491e8b5cfb55ea67b456a0b51bc951dba8b06
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-11-07 17:14:15 -0700

    commiting stuff

--- MMLessons.php
+++ MMLessons.php
@@ -18,20 +18,24 @@
 	<body>
 		<table>
 		
-		<?php
-			foreach ($lessons as $lesson) {
-				echo $lesson['gradek_5'];
-				/*
-				echo "<tr>
-							<td>Lesson: " . htmlentities($lesson['lessonname']) . "/></td>
-							<td>Content: " . if($lesson['gradek_5']){ . " K-5 ."}."</td>
-							<td>Description: ". htmlentities($lesson['description'])."</td>
-							<td>Attached File: ".{$lesson['lessonFile']}."</td>
-					</tr>";*/
-					
-			}
-		?>
-			
+			<?php
+				foreach ($lessons as $lesson) {
+					echo "<tr>
+								<td>Lesson: " . htmlentities($lesson['lessonname']) . "/></td>
+								<td>Description: ". htmlentities($lesson['description'])."</td>
+								<td>Attached File: ".{$lesson['lessonFile']}."</td>
+						</tr>";
+						
+				}
+			?>
+			<tr>
+				<td>
+				Lesson: will pull the lesson name from a submission.<br>
+				Content Level: will pull the level from a submission.<br>
+				Description: will pull the description from a submission.<br>
+				Attached File: will pull the file from a submission.<br>
+				</td>
+			</tr>
 		</table>
 		<?php require_once "MMFooter.php"; ?>
 	</body>
