b760ef831916f62fdf7f1e2b11c72c6017d4f556
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-11-07 17:52:24 -0700

    commiting stuff

--- MMLessons.php
+++ MMLessons.php
@@ -20,15 +20,15 @@
 		
 			<?php
 				foreach ($lessons as $lesson) {
-					$grade = "grade";
+					$grade = "";
 					if($lesson['gradek_5']==1){
 							$grade = "k-5";
 					}
 					if($lesson['grade6_8'] == 1){
-						$grade = $grade."6-8";
+						$grade = $grade." 6-8";
 					}
 					if($lesson['grade9_12']){
-						$grade = $grade."9-12";
+						$grade = $grade." 9-12";
 					}
 					
 					echo "<tr>
