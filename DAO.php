59178cfc6833479f3278671157379d4a5ec72a19
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-11-07 17:42:33 -0700

    commiting stuff

--- DAO.php
+++ DAO.php
@@ -76,7 +76,7 @@ class DAO {
 	
 	public function getLessons(){
 		$conn=$this->getConnection();
-		$q=$conn->prepare("select lessonname, gradek_5, grade6_8, grade9_12, description, lessonFile from lesson");
+		$q=$conn->prepare("select lessonname, gradek_5, grade6_8, grade9_12, description, lessonFile from lesson order by date_entered desc");
 		$q->setFetchMode(PDO::FETCH_ASSOC);
 		$q->execute();
 		$result=$q->fetchAll();
