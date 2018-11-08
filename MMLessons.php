1bb56a73b6087398ec2dc4d260a779784633ad59
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-11-07 17:26:27 -0700

    commiting stuff

--- MMLessons.php
+++ MMLessons.php
@@ -23,22 +23,12 @@
 					echo "<tr>
 							<td>
 								Lesson: " . htmlentities($lesson['lessonname']) . "<br>
-								Content Level: " . if($lesson['gradek_5'] == 1){ . "K-5" . } . "<br>
 								Description: " . htmlentities($lesson['description']) . "<br>
 								Attached File: " . $lesson['lessonFile'] . "<br><br>
 							</td>	
 						</tr>";
-						
 				}
 			?>
-			<tr>
-				<td>
-				Lesson: will pull the lesson name from a submission.<br>
-				Content Level: will pull the level from a submission.<br>
-				Description: will pull the description from a submission.<br>
-				Attached File: will pull the file from a submission.<br>
-				</td>
-			</tr>
 		</table>
 		<?php require_once "MMFooter.php"; ?>
 	</body>
