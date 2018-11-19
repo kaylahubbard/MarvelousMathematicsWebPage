c73d0f8d8be7f00099a6683f160929c8b6546599
Kayla Hubbard <kaylarhubbard@gmail.com>
2018-11-19 10:36:17 -0700

    commiting stuff

--- MMSubmitALesson.php
+++ MMSubmitALesson.php
@@ -23,8 +23,8 @@
 			?>
 			<?php } ?>
 			<form method=post action=MMSubmitALessonHandler.php>
-				Lesson Name:
-				<input type="text" name="lesson" value="<?php echo isset($_SESSION['presets']['lesson']) ? $_SESSION['presets']['lesson'] : ''; ?>"><br>
+				<label for="lessonName">Lesson Name:</label>
+				<input type="text" id="lessonName" name="lesson" value="<?php echo isset($_SESSION['presets']['lesson']) ? $_SESSION['presets']['lesson'] : ''; ?>"><br>
 				Good For Levels:
 				<label for="k5">K-5</label>
 				<input type="checkbox" id="k5" name="k-5" value="k-5">
@@ -32,7 +32,7 @@
 				<input type="checkbox" id="6_8" name="6-8" value="6-8">
 				<label for="9_12">9-12</label>
 				<input type="checkbox" name="9-12" id="9_12" value="9-12"><br>
-				Lesson Description:<br>
+				<label for="message">Lesson Description:</label><br>
 				<textarea id="message" name="description"><?php echo isset($_SESSION['presets']['description']) ? $_SESSION['presets']['description'] : ''; ?></textarea><br>
 				Submit:
 				<input type="submit" value="Submit" name="submit">
